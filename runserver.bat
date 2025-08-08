c433bd3b26b61075b2de6b8feb616612679b7913
Elyeet9 <goyasleonardo@gmail.com>
2025-08-08 16:09:35 -0500

    Added title to bat

--- runserver.bat
+++ runserver.bat
@@ -1,5 +1,6 @@
+@echo off
+title DnDownbeats Backend - Django
 echo "Starting Django development server..."
 echo "Access the server at: http://localhost:8008/"
 echo "Admin panel at: http://localhost:8008/admin/"
 python manage.py runserver 0.0.0.0:8008
-pause
